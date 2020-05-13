9b49185a5add3fed24b54a3bf370732b47199c51
Pierre Dramaix <pierre.dramaix.java@gmail.com>
2020-05-13 15:58:54 +0200

    update 13/05/2020

---
+++ Hello_World/src/Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+
+    public static void main(String[] args){
+         System.out.println("Hello World");
+        System.out.println("Hello Pierre");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = (5 + 10) + (2 * 10);
+        int myThirdNumber = 12;
+        int myFourthNumber = mySecondNumber *2;
+        int myTotal = myThirdNumber + myFourthNumber + mySecondNumber;
+
+        System.out.println(mySecondNumber);
+        System.out.println(myTotal);
+
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+        
+    }
+}
