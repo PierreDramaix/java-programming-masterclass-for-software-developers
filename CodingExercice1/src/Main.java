29e2af4d01c8712dcaf93814c2ff2a6eb45732d2
Pierre Dramaix <pierre.dramaix.java@gmail.com>
2020-05-19 16:18:05 +0200

    update 19/05/2020

---
+++ CodingExercice1/src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*SpeedConverter*/
+
+ /*       long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);*/
+
+        /*MegabyteConverter*/
+
+        /*MegaBytesConverter.printMegaBytesAndKiloBytes(2500);*/
+
+        /* BarkingDog */
+
+/*       boolean test = BarkingDog.shouldWakeUp(true, 1);
+        System.out.println(test);
+
+        test = BarkingDog.shouldWakeUp(false, 2);
+        System.out.println(test);
+
+        test = BarkingDog.shouldWakeUp(true, 8);
+        System.out.println(test);
+
+        test = BarkingDog.shouldWakeUp(true, -1);
+        System.out.println(test);*/
+
+        /*LeapYear*/
+
+/*      boolean  test = LeapYear.isLeapYear(-1600);
+        System.out.println(test);
+
+        test = LeapYear.isLeapYear(1600);
+        System.out.println(test);
+
+        test = LeapYear.isLeapYear(-2017);
+        System.out.println(test);
+
+        test = LeapYear.isLeapYear(2000);
+        System.out.println(test);
+
+        int test1 = 1924 % 4;
+        System.out.println(test1);
+
+        test1 = 1924 % 100;
+        System.out.println(test1);
+
+        test1 = 1924 % 400;
+        System.out.println(test1);*/
+
+        /*DecimalComparator*/
+
+/*       boolean test = DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175);
+        System.out.println(test);
+
+        test = DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176);
+        System.out.println(test);
+
+        test = DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0);
+        System.out.println(test);
+
+        test = DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123);
+        System.out.println(test);*/
+
+    }
+}
