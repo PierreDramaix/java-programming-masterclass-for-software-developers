7fd7527d1b8857885373ea8b40b338eb894b7a17
Pierre Dramaix <pierre.dramaix.java@gmail.com>
2020-12-08 12:11:27 +0100

    update 08/12/2020

---
+++ section 7 OOP part II composition, encapsulation and polymorphism/PolymorphismExercice/src/Car.java
@@ -0,0 +1,35 @@
+public class Car {
+
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine(){
+        return "Car engine is starting";
+    }
+
+    public String accelerate(){
+        return "Car is accelerating";
+    }
+
+    public String brake(){
+        return "Car is braking";
+    }
+}
